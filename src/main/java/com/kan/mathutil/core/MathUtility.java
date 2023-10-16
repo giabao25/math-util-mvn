a8c69a71047aa95e15b76b94270cb519a029bddb
giabao25 <kan250403@gmail.com>
2023-10-16 14:05:12 +0700

    Update getf() to see redalert

--- src/main/java/com/kan/mathutil/core/MathUtility.java
+++ src/main/java/com/kan/mathutil/core/MathUtility.java
@@ -22,7 +22,7 @@ public class MathUtility {
         
         //sống sót đến chỗ này, sure n = 2...20. Vì nếu ko chết dọc đường ở 
         // 2 cụm if ở trên rồi, KO CẦN ELSE NẾU CÓ RETURN
-        long result = 1; //giai thừa khởi đầu là 1
+        long result = 10; //giai thừa khởi đầu là 1
         //nhân dồn 2 3 4 5........n vào biến này thì thành n!
         
         for (int i = 2; i <= n; i++) {
